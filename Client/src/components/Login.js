5155c5c53d9f123f41b6b4cfb72565dfc0e3d13c
sheikh-sadik-vst-au4 <mohdsadik.sadik@gmail.com>
2020-04-03 12:35:58 +0530

    commmit sign in

--- Client/src/components/Login.js
+++ Client/src/components/Login.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import axios from 'axios';
-import { Redirect } from "react-router-dom";
+import { Redirect, Link } from "react-router-dom";
 
 export default class Login extends Component {
     constructor(props) {
@@ -24,31 +24,52 @@ export default class Login extends Component {
 
         axios.post(`http://localhost:5000/login`, { user })
             .then(res => {
-
                 if (res.data.status === 200) {
-                    localStorage.setItem('token', res.data.token)
+                    localStorage.setItem('token', res.data.token);
+                    localStorage.setItem('firstname', res.data.data.firstname);
+                    localStorage.setItem('id', res.data.data._id);
                     this.setState({
-                        loggedin:true    
+                        loggedin: true
                     })
+                }
+                else {
+                    alert("wrong password");
                 }
             })
     }
 
 
     render() {
-        if(this.state.loggedin)
-        {
+        if (this.state.loggedin) {
             return (
                 <Redirect to="/dashboard"></Redirect>
             )
         }
         return (
-            <div className="container">
-                <form onSubmit={this.Login}>
-                    <input type="text" onChange={(e) => this.setState({ username: e.target.value })}></input> <br></br><br></br>
-                    <input type="password" onChange={(e) => this.setState({ password: e.target.value })}></input><br></br><br></br>
-                    <input type="submit"></input>
-                </form>
+            <div className="auth-wrapper">
+                <div className="auth-inner mt-5">
+                    <form onSubmit={this.Login}>
+                        <h3>Sign In</h3>
+                        <div className="form-group">
+                            <input type="text" className="form-control" placeholder="Enter email"
+                                onChange={(e) => this.setState({ username: e.target.value })}>
+                            </input>
+                        </div>
+                        <div className="form-group">
+                            <input type="password" className="form-control" placeholder="Enter password"
+                                onChange={(e) => this.setState({ password: e.target.value })}>
+                            </input>
+                        </div>
+                        <input type="submit" className="btn btn-dark btn-block"></input>
+                        <p className="forgot-password text-right">
+                                Forgot password?
+                </p>
+                    </form>
+                    <hr></hr>
+                    <p className="forgot-password text-center">
+                                Dont have an account?<Link to="/signup">Sign up here</Link>
+                </p>
+                </div>
             </div>
         );
     }
--- Client/src/components/dashboard.js
+++ Client/src/components/dashboard.js
@@ -5,6 +5,8 @@ class Home extends Component {
     constructor(props) {
         super(props);
         const token = localStorage.getItem('token');
+        const name = localStorage.getItem('firstname');
+        const id = localStorage.getItem('id');
         let loggedin = true;
         if (token === null) {
             loggedin = false
@@ -13,14 +15,11 @@ class Home extends Component {
         this.state = {
 
             loggedin,
-            token
+            token,
+            name,
+            id
         }
     }
-
-    logout() {
-        localStorage.removeItem('token');
-    }
-
 
     render() {
         if (this.state.loggedin === false) {
@@ -30,12 +29,15 @@ class Home extends Component {
         }
 
         return (
-            <React.Fragment><nav className="navbar navbar-expand-lg navbar-light bg-light static-top">
+            <React.Fragment>< nav className="navbar navbar-expand-lg navbar-light bg-light static-top">
                 <div className="container-fluid">
                     <div className="navbar-nav mr-auto">
-                        <Link className="navbar-brand" to="/">testmania</Link>
+                        <Link className="navbar-brand" to="/dashboard">testmania</Link>
                     </div>
                     <ul className="navbar-nav ml-auto">
+                    <div className="navbar-nav mr-auto">
+        <Link className="navbar-brand text-capitalize" to="/user">hey {this.state.name}</Link>
+                    </div>
                         <li className="nav-item">
                             <Link to="/logout" className="nav-link text-dark link-button text-center">Logout</Link>
                         </li>
