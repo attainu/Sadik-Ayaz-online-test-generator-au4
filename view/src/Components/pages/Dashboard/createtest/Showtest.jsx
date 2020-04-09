62187ca3285c943445b006af77bde9f8f15282c5
sheikh-sadik-vst-au4 <mohdsadik.sadik@gmail.com>
2020-04-09 16:56:18 +0530

    done

--- view/src/Components/pages/Dashboard/createtest/Showtest.jsx
+++ view/src/Components/pages/Dashboard/createtest/Showtest.jsx
@@ -1,57 +1,74 @@
 import React from "react";
 const Showtest = (props) => {
   return (
-    <div className="card">
-      <h3 className="mt-3 text-capitalize text-danger">{props.testName}</h3>
-      {props.testQuestion.map((item, index) => {
-        return (
-          <div className="card col-10 mt-3 mx-auto" key={index}>
-            <div className="card-body">
-              <div className="row">
-                <div className="col-md-1 text-left">
-                  <label className="lable-class ">{index + 1}.</label>
+    <div className="card mb-5">
+      <div className="container-fluid">
+        <div className="row mt-3">
+          <div className="col text-capitalize text-dark text-left">
+            <h5>title - {props.testName}</h5>
+          </div>
+          <div className="col  text-capitalize text-dark text-right">
+            <h5> total marks : 20</h5>
+          </div>
+        </div>
+        <hr></hr>
+        <div className=" row mb-3">
+        {props.testQuestion.map((item, index) => {
+          return (
+            <div className="card col-10 mt-3 mx-auto" key={index}>
+              <div className="card-body">
+                <div className="row">
+                  <div className="col-md-1 text-left">
+                    <label className="lable-class ">{index + 1}.</label>
+                  </div>
+                  <div className="col-md-11">
+                    <p className="text-left">{item.question}</p>
+                    <i class="fas fa-edit"></i>
+                  </div>
                 </div>
-                <div className="col-md-11">
-                  <p className="text-left">{item.question}</p>
-                  <i class="fas fa-edit"></i>
+                <div className="row">
+                  <div className="col-md-1 text-left">
+                    <label className="lable-class ">A.</label>
+                  </div>
+                  <div className="col-md-11 text-left">
+                    <p>{item.options.option1}</p>
+                  </div>
                 </div>
-              </div>
-              <div className="row">
-                <div className="col-md-1 text-left">
-                  <label className="lable-class ">A.</label>
+                <div className="row">
+                  <div className="col-md-1 text-left">
+                    <label className="lable-class ">B.</label>
+                  </div>
+                  <div className="col-md-11 text-left">
+                    <p>{item.options.option2}</p>
+                  </div>
                 </div>
-                <div className="col-md-11 text-left">
-                  <p>{item.options.option1}</p>
+                <div className="row">
+                  <div className="col-md-1 text-left">
+                    <label className="lable-class ">C.</label>
+                  </div>
+                  <div className="col-md-11 text-left">
+                    <p>{item.options.option3}</p>
+                  </div>
                 </div>
-              </div>
-              <div className="row">
-                <div className="col-md-1 text-left">
-                  <label className="lable-class ">B.</label>
-                </div>
-                <div className="col-md-11 text-left">
-                  <p>{item.options.option2}</p>
-                </div>
-              </div>
-              <div className="row">
-                <div className="col-md-1 text-left">
-                  <label className="lable-class ">C.</label>
-                </div>
-                <div className="col-md-11 text-left">
-                  <p>{item.options.option3}</p>
-                </div>
-              </div>
-              <div className="row">
-                <div className="col-md-1 text-left">
-                  <label className="lable-class ">D.</label>
-                </div>
-                <div className="col-md-11 text-left">
-                  <p>{item.options.option4}</p>
+                <div className="row">
+                  <div className="col-md-1 text-left">
+                    <label className="lable-class ">D.</label>
+                  </div>
+                  <div className="col-md-11 text-left">
+                    <p>{item.options.option4}</p>
+                  </div>
                 </div>
               </div>
             </div>
+          );
+        })}
+      </div>
+        <div className="row">
+          <div className="col-3 mx-auto mb-3">
+            <button className="btn btn-block btn-success">Finish Test</button>
           </div>
-        );
-      })}
+          </div> 
+      </div>
     </div>
   );
 };
