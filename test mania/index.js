3b3ff4821fa8e23ac21b695f1a7ff3f9f10e2e43
sheikh-sadik-vst-au4 <mohdsadik.sadik@gmail.com>
2020-04-25 21:45:26 +0530

    added

--- test mania/index.js
+++ test mania/index.js
@@ -4,11 +4,19 @@ const cors = require('cors');
 const PORT = process.env.PORT || 5000;
 const router = require('./router');
 const connectDB = require('./database');
+const path = require('path');
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/', router);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname,'client','build','index.html'))
+    })
+}
 
 
 connectDB().then(() => {
